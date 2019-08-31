bd2ee85acaa6697226181dff2401bf38f85d7ba7
Jeremie J. Jarosh <jeremie@jarosh.org>
2019-08-31 11:07:02 -0500

    Revert "[audible:library] Remove extractor, for now"
    This reverts commit e8d52503ca050402c83a1843527a9ad98be2027a.

--- youtube_dl/extractor/audible.py
+++ youtube_dl/extractor/audible.py
@@ -285,3 +285,6 @@ class AudibleIE(InfoExtractor):
             'description': description if description is not "" else None,
             # TODO more properties (see youtube_dl/extractor/common.py)
         }
+
+class AudibleLibraryIE(InfoExtractor):
+    IE_NAME = 'audible:library'
